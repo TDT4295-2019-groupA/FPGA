491da666ba4c64991ddebcd619b049e5a2673833
Peder Bergebakken Sundt <pbsds@hotmail.com>
2019-09-29 11:20:24 +0200

    A more interesting spi pattern to test

--- spi_test.py
+++ spi_test.py
@@ -9,13 +9,22 @@ CHIP_SELECT = 0
 
 spi = spidev.SpiDev()
 spi.open(0, CHIP_SELECT)
-spi.max_speed_hz = 10000
+spi.max_speed_hz = 100000
 spi.mode = 0
 spi.bits_per_word = 8
 spi.cshigh = True
 
+seq = [
+    0b1000,
+    0b0100,
+    0b0010,
+    0b0001,
+    0b0010,
+    0b0100,
+]
+
 while 1:
-    for i in range(0x10):
+    for i in seq:
         print(f"Sending 0x{i:x}...")
         spi.xfer([i])
-        time.sleep(0.8)
+        time.sleep(0.1)
