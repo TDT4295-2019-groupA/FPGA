9f87a3d719dad30327e0b03006fd99f7d010afce
solbjorg <1645846+solbjorg@users.noreply.github.com>
2019-10-09 17:08:46 +0200

    Fixed upload script so it works on any Arty

--- scripts/upload_bitfile.sh
+++ scripts/upload_bitfile.sh
@@ -18,7 +18,7 @@ colorize tee "$TMP" <<- EOT
 
 	# Connect to the Digilent Cable on localhost:3121
 	connect_hw_server -url localhost:3121
-	current_hw_target [get_hw_targets */xilinx_tcf/Digilent/210319A28ED8A]
+	current_hw_target [get_hw_targets */xilinx_tcf/Digilent/*]
 	open_hw_target
 
 	# Program and Refresh the xc7a35t_0 Device
