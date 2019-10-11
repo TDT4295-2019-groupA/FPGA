cbd1b20dc8af9e46c6af364055bb2558c8da652f
Peder Bergebakken Sundt <pbsds@hotmail.com>
2019-10-12 00:39:59 +0200

    Expand PWM to support setting step size

--- src/main/scala/common/PWM.scala
+++ src/main/scala/common/PWM.scala
@@ -3,14 +3,14 @@ package common
 import chisel3._
 import chisel3.experimental.MultiIOModule
 
-class PWM(val width:Int = 8) extends MultiIOModule {
+class PWM(val width:Int = 8, val step:BigInt = 1) extends MultiIOModule {
   val io = IO(new Bundle{
     val high   = Output(Bool())    // Data Valid pulse (1 clock cycle)
     val target = Input(UInt(width.W))
   })
 
 	val counter = RegInit(UInt(width.W), 0.U)
-	counter := counter + 1.U
+	counter := counter + step.U
 	when (counter > io.target) {
 		io.high := false.B
 	} otherwise {
