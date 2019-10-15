fb65731dbab99087ae38286512477b3e39aefa41
Amund Bergland Kvalsvik <Oracien@github.com>
2019-10-15 17:50:38 +0200

    Fixed according to reference implementation. Kinda broken

--- src/main/scala/generator/Generator.scala
+++ src/main/scala/generator/Generator.scala
@@ -26,10 +26,10 @@ class Generator extends MultiIOModule{
 
   // LUTs
   def fpga_note_index_to_freq(note_index: Int): Double =
-    config.MIDI_A3_FREQ * scala.math.pow(2.0, (note_index - config.MIDI_A3_INDEX) / 12.0)
+    math.round((1 << config.FREQ_SHIFT) * config.MIDI_A3_FREQ * math.pow(2.0, (note_index - config.MIDI_A3_INDEX) / 12.0))
 
-  def freq_to_wavelength_in_samples(freq: Double): Double =
-    scala.math.round(config.SAMPLE_RATE / freq)
+  def freq_to_wavelength_in_samples(freq: UInt): UInt =
+    ((config.SAMPLE_RATE << config.FREQ_SHIFT) * config.NOTE_LIFE_COEFF).asUInt() / freq
 
 
   /*
@@ -41,22 +41,34 @@ class Generator extends MultiIOModule{
    */
 
   val lookup_value_array = new Array[(UInt, UInt)](12)
-  val note_remainder: UInt = Wire(UInt())
-  val note_divide: UInt = Wire(UInt())
-  val wavelength_base: UInt = Wire(UInt())
+  val note_offset: UInt = Wire(UInt(16.W))
+  val note_remainder: UInt = Wire(UInt(16.W))
+  val note_divide: UInt = Wire(UInt(16.W))
+  val wavelength_base: UInt = Wire(UInt(16.W))
+  val freq: UInt = Wire(UInt(16.W))
+  val freq_base: UInt = Wire(UInt(16.W))
+  val freq_coeff: UInt = Wire(UInt(16.W))
 
   note_remainder := generator_config.note_index % 12.U
   note_divide := generator_config.note_index / 12.U
   wavelength_base := 0.U
 
-  wavelength_base := DontCare
+
+  freq_base := DontCare
+
   for (i <- 0 until 11) {
     when (note_remainder === i.U) {
-      wavelength_base := freq_to_wavelength_in_samples(fpga_note_index_to_freq(i)).toInt.U
+      freq_base := (fpga_note_index_to_freq(i).toInt.U >> note_divide).asUInt()
     }
   }
 
-  val wavelength = wavelength_base >> note_divide //(MuxLookup(1.U, 0.U (32.W), lookup_value_array) >> note_divide).asUInt()
+  note_offset := (2.U * io.global_config.pitchwheels(generator_config.channel_index)) / 128.U
+  //This is bad and dumb, replace with lookup table
+  freq_coeff := (2.U << (note_offset /12.U)).asUInt() * (1 << config.FREQ_SHIFT).asUInt()
+
+  freq := (freq_base * freq_coeff) >> config.FREQ_SHIFT
+
+  val wavelength = freq_to_wavelength_in_samples(freq)
 
   // handle input
   when (io.generator_update_valid) {
@@ -74,7 +86,6 @@ class Generator extends MultiIOModule{
       wavelength_pos := wavelength_pos + 1.U
     }
   }
-
 
   val current_sample = Wire(SInt(16.W))
   current_sample := 0.S
