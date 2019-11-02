87355fe18cc60b2235feeb202ab998a027c7df75
solbjorg <1645846+solbjorg@users.noreply.github.com>
2019-11-02 12:15:49 +0100

    clocks work, but we havent made things run clocked yet :sadieparrot:

--- src/main/scala/blackboxes/MMCM.scala
+++ src/main/scala/blackboxes/MMCM.scala
@@ -6,24 +6,6 @@ import chisel3.core.{DoubleParam, IntParam, StringParam}
 import chisel3.experimental.MultiIOModule
 import chisel3.experimental.ExtModule
 
-class MMCM extends MultiIOModule {
-  val io = IO(new Bundle{
-    val clk_in = Input(Clock())
-    val clk_out1 = Output(Clock())
-  })
-
-  val mmcm = Module(new MMCM_clk_wiz())
-  mmcm.clk_in := io.clk_in
-  io.clk_out1 := mmcm.clk_out1
-}
-
-// Blackbox for verilog module, along with its shitty names
-class MMCM_clk_wiz extends ExtModule() { // Verilog parameters
-  override def desiredName: String = "MMCM_clk_wiz" // verilog name
-  val clk_in = IO(Input(Clock()))
-  val clk_out1 = IO(Output(Clock()))
-}
-
 case class ClockConfig(divide: Int, duty_cycle: Double, phase: Double)
 
 object ClockConfig {
@@ -34,7 +16,6 @@ class MMCME2(
               period: Double,
               base_mult: Double,
               base_div: Int,
-              clock0_divide: Double,
               clocks: List[ClockConfig],
               clock4_cascade: Boolean
             ) extends ExtModule(Map(
