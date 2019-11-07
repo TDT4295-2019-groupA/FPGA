2ec9781330a98b3e8fa9a1f46244c117979c180b
Robert Maikher <robertem342@gmail.com>
2019-11-07 16:07:50 +0100

    More state testing

--- src/main/scala/top.scala
+++ src/main/scala/top.scala
@@ -1,15 +1,21 @@
 package sadie.generator
 
 import chisel3._
+import chisel3.util.Counter
 
 class TopBundle extends Bundle {
   val gpio = Output(UInt(1.W))
   val gpio2 = Output(UInt(1.W))
+  val gpio3 = Output(UInt(1.W))
 }
 
 class Top() extends Module {
+  val counter = Counter(100000000)
+  counter.inc()
   val io = IO(new TopBundle)
   val reggie = RegInit(Bool(), true.B)
+  val reggie2 = RegInit(Bool(), false.B)
   io.gpio := reggie
-  io.gpio2 := 1.U
+  io.gpio2 := reggie2
+  io.gpio3 := counter.value === 0.U
 }
