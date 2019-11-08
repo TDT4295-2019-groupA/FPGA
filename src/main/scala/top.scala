259f690b62ee535e88290257bb8d36ef1081f4b1
solbjorg <1645846+solbjorg@users.noreply.github.com>
2019-11-08 13:37:34 +0100

    THE LIGHT BLINKS :)

--- src/main/scala/top.scala
+++ src/main/scala/top.scala
@@ -10,12 +10,18 @@ class TopBundle extends Bundle {
 }
 
 class Top() extends Module {
-  val counter = Counter(100000000)
-  counter.inc()
+  def counter() = {
+    val x = RegNext(0.asUInt(128.W))
+    x := x + 1.U
+    x
+  } 
+  val init_counter = Counter(100000000)
+  val next_counter = counter()
+  init_counter.inc()
   val io = IO(new TopBundle)
   val reggie = RegInit(Bool(), true.B)
   val reggie2 = RegInit(Bool(), false.B)
-  io.gpio := reggie
-  io.gpio2 := reggie2
-  io.gpio3 := counter.value === 0.U
+  io.gpio := next_counter >= 16000000.U 
+  io.gpio2 := next_counter === 0.U
+  io.gpio3 := init_counter.value >= 10000000.U
 }
