d99d23da93f9c7ab547384f6f46738b247cb2d75
Amund Bergland Kvalsvik <amundbk+github@gmail.com>
2019-11-18 11:06:58 +0100

    Take this test module adventurer

--- src/main/scala/spi_chisel.scala
+++ src/main/scala/spi_chisel.scala
@@ -12,12 +12,17 @@ class SpiChisel extends MultiIOModule {
 
       val spi_package_out = Output(UInt())
       val spi_package_ready = Output(Bool())
+
+      val spi_test_out = Output(UInt(1.W))
+      val spi_test_clock_out = Output(Bool())
     }
   )
 
   val lastClock = RegInit(Bool(), false.B)
   val dataStore = RegInit(UInt(256.W), 0.U)
   val wasActive = RegInit(Bool(), false.B)
+
+  val testOut = RegInit(UInt(72.W), 0.U)
 
   io.spi_package_out := dataStore
 
@@ -28,11 +33,17 @@ class SpiChisel extends MultiIOModule {
     }
     .elsewhen(lastClock =/= io.spi_clock && io.spi_clock) {
       dataStore := (dataStore << 1) ^ io.spi_databit_in
+      testOut := testOut << 1
     }
     lastClock := io.spi_clock
     io.spi_package_ready := false.B
   }.otherwise {
+    testOut := dataStore
     io.spi_package_ready := dataStore =/= 0.U
     wasActive := false.B
   }
+
+  io.spi_test_out := testOut(71)
+  io.spi_test_clock_out := io.spi_clock
+
 }
