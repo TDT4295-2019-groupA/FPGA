2034bed7a874d4d14e76022cb0d07505f0e208dc
solbjorg <1645846+solbjorg@users.noreply.github.com>
2019-11-19 12:54:49 +0100

    Flipped CS in SPI, curse you peder. Also more clocking tests.

--- src/main/scala/top.scala
+++ src/main/scala/top.scala
@@ -25,7 +25,7 @@ class Top() extends MultiIOModule {
     ClockConfig.default, 
     ClockConfig.default, 
     ClockConfig.default,
-    ClockConfig(100, 0.5, 0.0), //For BCK
+    ClockConfig(50, 0.5, 0.0), //For BCK
     ClockConfig.default,
     ClockConfig(6, 0.5, 0.0), //For SCK
   )
@@ -36,8 +36,8 @@ class Top() extends MultiIOModule {
   mmcm.CLKFBIN := mmcm.CLKFBOUT
   mmcm.PWRDWN := false.B
   mmcm.RST := false.B
-  val ChillClock = mmcm.CLKOUT4
-  val PWMClock = mmcm.CLKOUT6 
+  val ChillClock = mmcm.CLKOUT4 // like 2MHz lol
+  val PWMClock = mmcm.CLKOUT6 // like 100MHz
   // initalize top-modules
   val saved_sample = RegInit(SInt(32.W), 0.S)
   withClock(ChillClock) {
@@ -65,7 +65,7 @@ class Top() extends MultiIOModule {
     rx.TX_data_valid := false.B // transmit nothing
     rx.TX_data := 0.U
     rx.spi <> io.spi // connect spi slave bus to io 
-      //do this usually
+    //do this usually
     //i2s.sound := sound.sample_out 
       // drive the input handler module
     input.RX_data       := rx.RX_data
@@ -89,6 +89,6 @@ class Top() extends MultiIOModule {
     val pwm = Module(new PWM(32, 0x30303030)).io
     io.pwm_out_l := pwm.high 
     io.pwm_out_r := pwm.high 
-    pwm.target := ((saved_sample << 13.U) + 0x80000000.S(33.W) ).asUInt
+    pwm.target := (saved_sample + 0x80000000.S(33.W) ).asUInt
   }
 }
